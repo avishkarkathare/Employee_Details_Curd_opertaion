3a8bf58533ead04498f2ad3f00b68e86ea5a623d
Avishkar Kathare <109351945+avishkarkathare@users.noreply.github.com>
2024-06-04 19:28:00 +0530

    Employee_Application

---
+++ EmpCurdsApplication.java
@@ -0,0 +1,14 @@
+package com.emp_curdss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpCurdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpCurdsApplication.class, args);
+		System.out.println("heelo");
+	}
+
+}
